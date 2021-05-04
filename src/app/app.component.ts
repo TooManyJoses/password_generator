02be33c693035b179a70f3dc63aad4834e026f7c
Iron.Snowflake <jadrubio@gmail.com>
2021-05-04 19:38:47 -0400

    created basic passowrd generator and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwd-generator';
+  passwordLength: number = 0;
+  generatedPassword: string = '';
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+
+  enterLength(length: string) {
+    const parsedValue = parseInt(length);
+    if(!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue
+    }
+  }
+
+  setUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  setUseNumbers() {
+    this.useNumbers = !this.useNumbers
+  }
+
+  setUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onGenerate() {
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.useLetters){
+      validChars += letters
+    }
+
+    if(this.useNumbers){
+      validChars += numbers
+    }
+
+    if(this.useSymbols){
+      validChars += symbols
+    }
+
+    let createPassword = '';
+
+    for(let i=0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      createPassword += validChars[index];
+    }
+
+    this.generatedPassword = createPassword;
+  }
+
+  getPassword() {
+    return this.generatedPassword;
+  }
 }
